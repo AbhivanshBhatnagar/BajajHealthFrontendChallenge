c323dc4d3ead2506dfea7217443ed55f05fdac80
AbhivanshBhatnagar <bhatnagar1102abhivansh@gmail.com>
2023-05-19 22:26:19 +0530

    refactoring

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:bajaj_frontend_challenge/home.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
+}
