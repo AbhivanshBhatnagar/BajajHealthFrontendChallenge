5c74519e960006a59193aef499781daa1a5e744b
AbhivanshBhatnagar <bhatnagar1102abhivansh@gmail.com>
2023-05-20 06:43:24 +0530

    some refactoring changes

--- lib/app.dart
+++ lib/app.dart
@@ -11,7 +11,7 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Home(),
     );
   }
--- lib/home.dart
+++ lib/home.dart
@@ -1,15 +1,86 @@
+import 'package:bajaj_frontend_challenge/components/components.dart';
+import 'package:bajaj_frontend_challenge/components/loading.dart';
+import 'package:bajaj_frontend_challenge/models/dataModel.dart';
+import 'package:bajaj_frontend_challenge/models/networkingModel.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatefulWidget {
-  const Home({super.key});
-
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  List<Employee> _users = <Employee>[];
+  List<Employee> _usersDisplay = <Employee>[];
+
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUsers().then((value) {
+      setState(() {
+        _isLoading = false;
+        _users.addAll(value);
+        _usersDisplay = _users;
+        print(_usersDisplay.length);
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text('Users List'),
+      ),
+      body: SafeArea(
+        child: Container(
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              if (!_isLoading) {
+                return index == 0
+                    ? _searchBar()
+                    : UserTile(employee: _usersDisplay[index - 1]);
+              } else {
+                return LoadingView();
+              }
+            },
+            itemCount: _usersDisplay.length + 1,
+          ),
+        ),
+      ),
+    );
+  }
+
+  _searchBar() {
+    return Padding(
+      padding: EdgeInsets.all(12.0),
+      child: TextField(
+        autofocus: false,
+        onChanged: (searchText) {
+          searchText = searchText.toLowerCase();
+          setState(() {
+            _usersDisplay = _users.where((u) {
+              var name = u.name != null ? u.name!.toLowerCase() : "Unknown";
+              var designation = u.designation != null
+                  ? u.designation!.toLowerCase()
+                  : "Unknown";
+              return name.contains(searchText) ||
+                  designation.contains(searchText) ||
+                  designation.contains(searchText);
+            }).toList();
+          });
+        },
+        // controller: _textController,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          prefixIcon: Icon(Icons.search),
+          hintText: 'Search Users',
+        ),
+      ),
+    );
   }
 }
